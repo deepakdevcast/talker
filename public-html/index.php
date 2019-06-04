5fb98d425100f01d9fd090869ca903cfa4c8a4ca
deepakjuit <dk8232847@gmail.com>
2019-06-05 03:22:45 +0530

    Bootstrap starter template

--- public-html/index.php
+++ public-html/index.php
@@ -1,82 +1,21 @@
-<?php 
-$website="http://www.google.com";
-?>
 <!DOCTYPE html>
-<head>
-    <title>hello world!</title>
-</head>
-<body>
-    <h1>Hello world!</h1>
-    <a href="<?php echo $website;?>"><?php echo "string deepak $website";?></a>
-    <p>
-    <?php 
-    $contries = ['germany','france','spain'];
-    print_r($contries);
-    ?>
-    </p>
-    <p>
-    <?php
-    $contries[]='india';
-    print_r($contries);
-    ?>
-    </p>
-    <p>
-    <?php print_r($contries[3]);
-       echo count($contries);
-    ?>
-    </p>
-    <p>
-    <?php
-    $age = array(
-        'John'=>35,
-        'Paul'=>24,
-        'George'=> 27
-    );
-    print_r($age);
-    ?>
-    </p>
-    <p>
-    <?php 
-        class carBluePrint{
-         //properties
-         //constructor
-         public function __construct($newColor,$newMake)
-         {
-             $this->color=$newColor;
-             $this->make=$newMake;
-         }
-         //setter method
-         public function setColor($newColor){
-             $this->color = $newColor;
-         }
-         public function getColor(){
-             return "<br>new color is: ".$this->color."<br>";
-         }
-        }
-        $firstRealColor = new carBluePrint("green","Volvo");
-        echo $firstRealColor->getColor();
-        var_dump($firstRealColor);
-        $secondRealColor = new carBluePrint("brown","Mercedes");
-        echo $secondRealColor->getColor();
-        var_dump($secondRealColor);
-    ?>
-    </p>
-    <p>
-    <?php
-       class sportCarBluePrint extends carBluePrint{
-           public function __construct($newColor,$newMake,$newSpoiler){
-               parent::__construct($newColor,$newMake);
-               $this->spoiler=$newSpoiler;
-           }
-           public function activateSpoiler(){
-               return "<br><strong>SPOILER ACTIVATED</strong><br>";
-           }
-       }
-       $firstSportCar=new sportCarBluePrint("maginta","Porsche","tail");
-       $firstSportCar->setColor("pink");
-       echo $firstSportCar->activateSpoiler();
-       var_dump($firstSportCar);
-    ?>
-    </p>
-</body>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TALKER</title>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>TALKER</h1>
+
+    <!-- Optional Javascript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
 </html>
