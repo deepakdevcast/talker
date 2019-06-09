55930445fd434ae3c60f30c9a5350d439ebd8306
deepakjuit <dk8232847@gmail.com>
2019-06-09 16:05:37 +0530

    Update system.ctrl.php

--- public-html/system.ctrl.php
+++ public-html/system.ctrl.php
@@ -8,7 +8,10 @@ function phpShowFeedback($feedback_id) {
 		$feedback_type="danger";
 		$feedback_text="Passwords don't match";
 		break;
-
+        case "804":
+        $feedback_type="danger";
+        $feedback_text="Email is already exist!!";
+        break;
 		case "811":
 		$feedback_type="success";
 		$feedback_text="you have successfully Sign Up";
@@ -40,4 +43,16 @@ function phpModifyDB($db_query, $db_data) {
     $statement = $connection->prepare($db_query);
     $statement->execute($db_data);
 }
+// Get the information from the database
+function phpFetchDB($db_query, $db_data) {
+    global $connection;
+
+    $statement = $connection->prepare($db_query);
+    $statement->execute($db_data);
+
+    //setting the fetch mode and returning the result
+    return $statement->fetch(PDO::FETCH_ASSOC);
+}
+
+
 ?>
