ab2c040cf0cd6f4e099bd8fa3af6d26062e6430f
deepakjuit <dk8232847@gmail.com>
2019-06-09 16:05:40 +0530

    Update signup.ctrl.php

--- public-html/signup.ctrl.php
+++ public-html/signup.ctrl.php
@@ -15,11 +15,22 @@
     $password_validation=preg_match($user_password_pattern,$user_password);
 
     //checking of input in server-sides
-    
+
     if ($email_validation && $password_validation && $user_password == $_POST["formSignUpPasswordConf"]) {
-        $db_data=array($user_email,$user_password);
-        phpModifyDB('INSERT INTO users (user_email,user_password) values(?,?)',$db_data);
-        $_SESSION["msgid"]="811";
+        //checking if the submitted email is already in users table
+	$db_data = array($user_email);
+	$isAlreadySignedUp = phpFetchDB('SELECT user_email FROM users WHERE user_email = ?', $db_data);
+	$db_data = "";
+
+	//if no result is returned, insert new record to the table, otherwise display feedback
+	if (!is_array($isAlreadySignedUp)) {
+		$db_data = array($user_email, $user_password);
+		phpModifyDB('INSERT INTO users (user_email, user_password) values (?, ?)', $db_data);
+		$db_data = "";
+		$_SESSION["msgid"] = "811";
+	}else{
+		$_SESSION["msgid"] = "804";
+	}
         header('location: index.php');
     }  else if (!$email_validation) {
         $_SESSION["msgid"]="801";
